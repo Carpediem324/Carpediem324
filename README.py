c812b7450dd4b2a2cfaa413c95a8e118b4032970
Carpediem324 <101110020+Carpediem324@users.noreply.github.com>
2023-03-31 14:37:47 +0900

    Update README.py

--- README.py
+++ README.py
@@ -3,4 +3,4 @@ learn Computer Science and Engineering
 
 interested in artificial intelligence, IoT, embedded systems, V2X, and autonomous vehicles.
 
-use c,c++,JAVA,PYTHON
+use c,c++,PYTHON,JavaScript...

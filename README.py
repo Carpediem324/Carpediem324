870dd99cdba39cd57ee49f240933fb8a8572401f
Carpediem324 <101110020+Carpediem324@users.noreply.github.com>
2024-02-01 14:07:30 +0900

    Update README.py

--- README.py
+++ README.py
@@ -1,6 +1,6 @@
 Korean student.
 learn Computer Science and Engineering
 
-interested in artificial intelligence, IoT, embedded systems, V2X, and autonomous vehicles.
+SDV, Autonomous Driving, Computer Vision, Localization
 
 use c,c++,PYTHON,JavaScript...

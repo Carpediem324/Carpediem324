7f5e8c16ae104b07b1be66d8b7debd9d0c36fbf5
Carpediem324 <101110020+Carpediem324@users.noreply.github.com>
2024-07-13 17:05:23 +0900

    Update README.md

--- README.py
+++ README.py
@@ -1,6 +1,19 @@
-Korean student.
-learn Computer Science and Engineering
+## Hello! 👋
 
-SDV, Autonomous Driving, Computer Vision, Localization
+I am a junior robot programmer and developer from South Korea.  
+I am studying Computer Science and Engineering and have been involved in various projects and research.
 
-use c,c++,PYTHON,JavaScript...
+### Education 🎓
+- **Korea University of Technology and Education**  
+  Bachelor of Computer Engineering
+
+### Experience and Activities 🚀
+- Internship at Korea Atomic Energy Research Institute (KAERI)
+- SAMSUNG SW ACADEMY FOR YOUTH (SSAFY) (In Progress)
+
+### Skills 🛠️
+- **Programming Languages:** C, C++, Python, JavaScript
+- **Fields of Interest:** SDV, Autonomous Driving, Computer Vision, Localization, SLAM, ROS2
+
+### About Me 💡
+I am passionate about robotics, autonomous driving, and localization. I constantly seek to improve my skills and contribute to innovative projects in these fields.
